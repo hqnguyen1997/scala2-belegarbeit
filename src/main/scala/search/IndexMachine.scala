90c71ecc501954fdb66723661f5ce8181bd34ddc
s0566212 <hquan.nguyen1997@gmail.com>
2020-08-20 23:14:49 +0200

    refactor comments

--- src/main/scala/search/IndexMachine.scala
+++ src/main/scala/search/IndexMachine.scala
@@ -10,38 +10,44 @@ import utils.{LangCode, LanguageDetector, Tokenizer}
 class IndexMachine {
 
   def createIndex(dataSource: String, indexOutput: String, sc: SparkContext): InvertedIndex = {
-
     val csvData = sc.textFile(dataSource)
     val preferredLanguages = List(LangCode.GERMAN, LangCode.ENGLISH)
 
     val rows: RDD[(String, String)] = csvData.map(line => {
-      //Zeilen lesen
+      // Read lines
       val splitted: Array[String] = line.split(";")
-      //aus der Zeile Vorkommenshäufigkeit und Url entnehmen
-      if (splitted.size == 2) (splitted(0), splitted(1)) else ("", "")
+      // Get url
+      if (splitted.size == 2) {
+        (splitted(0), splitted(1))
+      } else {
+        ("", "")
+      }
     })
 
+    /**
+     * -- Tokenize text => (url, token)
+     * -- Determine language => (url, token, language_code)
+     * -- Filter records, which are in preferred language
+     * -- Filter stop word
+     * -- Stemming => (url, stemmed_token)
+     * -- Caculate term frequency
+     *
+     */
     val preparedData: RDD[(String, Map[String, Int])] =
       rows
-        // (url, token)
         .map(rec => (rec._1, Tokenizer.tokenize(rec._2)))
-        //url , tokens , language code
         .map(rec => (rec._1, rec._2, LanguageDetector.detect(rec._2)))
-        //nur unsere bevorzugten sprachen werden herausgenommen
         .filter(preferredLanguages contains _._3)
-        //Stopworte werden gefiltert
         .map(rec => (rec._1, StopwordFilter.filter(rec._2, rec._3), rec._3))
-        //url, stemmedTokens
         .map(rec => (
           rec._1,
           if (rec._3 == LangCode.GERMAN) rec._2.map(word => GermanStemmer.stem(word))
           else rec._2.map(word => EnglishStemmer.stem(word))
         ))
-        //term frequency wird ermittelt
         .map(rec => (rec._1 -> Tf.tf(rec._2)))
 
     val invertedIndex = new InvertedIndex(preparedData)
-    // Index wird gespeichert
+
     this.saveIndex(invertedIndex, indexOutput)
 
     println("Datei gespeichert:" + indexOutput)
